12231c77d3a97d51435bc1d416bd6548db773d74
mindsear <xxusername12843@yahoo.ro>
2018-05-21 23:44:43 +0300

    AddRegEnvironmentVar First Commit

---
+++ AddRegKey/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace AddRegKey
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAddRegKey = new System.Windows.Forms.Button();
+            this.txtBoostPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.variableName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAddRegKey
+            // 
+            this.btnAddRegKey.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnAddRegKey.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnAddRegKey.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddRegKey.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnAddRegKey.ForeColor = System.Drawing.Color.White;
+            this.btnAddRegKey.Location = new System.Drawing.Point(232, 117);
+            this.btnAddRegKey.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddRegKey.Name = "btnAddRegKey";
+            this.btnAddRegKey.Size = new System.Drawing.Size(103, 31);
+            this.btnAddRegKey.TabIndex = 0;
+            this.btnAddRegKey.Text = "Add/Replace";
+            this.btnAddRegKey.UseVisualStyleBackColor = false;
+            this.btnAddRegKey.Click += new System.EventHandler(this.btnAddRegKey_Click);
+            this.btnAddRegKey.MouseEnter += new System.EventHandler(this.btnAddRegKey_MouseEnter);
+            this.btnAddRegKey.MouseLeave += new System.EventHandler(this.btnAddRegKey_MouseLeave);
+            // 
+            // txtBoostPath
+            // 
+            this.txtBoostPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBoostPath.Location = new System.Drawing.Point(142, 78);
+            this.txtBoostPath.Margin = new System.Windows.Forms.Padding(4);
+            this.txtBoostPath.Name = "txtBoostPath";
+            this.txtBoostPath.Size = new System.Drawing.Size(306, 25);
+            this.txtBoostPath.TabIndex = 1;
+            this.txtBoostPath.Text = "C:\\local\\boost_1_65_1";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Boost Path:";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnBrowse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBrowse.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBrowse.ForeColor = System.Drawing.Color.White;
+            this.btnBrowse.Location = new System.Drawing.Point(456, 78);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(73, 25);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            this.btnBrowse.MouseEnter += new System.EventHandler(this.btnBrowse_MouseEnter);
+            this.btnBrowse.MouseLeave += new System.EventHandler(this.btnBrowse_MouseLeave);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.Gainsboro;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(483, 163);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 24);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // variableName
+            // 
+            this.variableName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.variableName.Location = new System.Drawing.Point(142, 39);
+            this.variableName.Name = "variableName";
+            this.variableName.Size = new System.Drawing.Size(306, 25);
+            this.variableName.TabIndex = 5;
+            this.variableName.Text = "BOOST_ROOT";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Variable Name:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(454, 41);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 21);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtStatus.Location = new System.Drawing.Point(12, 194);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtStatus.Size = new System.Drawing.Size(543, 79);
+            this.txtStatus.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label3.Location = new System.Drawing.Point(430, 287);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "coded by mindsear";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 308);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.variableName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoostPath);
+            this.Controls.Add(this.btnAddRegKey);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Boost Environment Variables";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddRegKey;
+        private System.Windows.Forms.TextBox txtBoostPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox variableName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ AddRegKey/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace AddRegKey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddRegKey_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                AddUserVariable();
+
+                AddSystemVariable();
+
+                string btnText = btnAddRegKey.Text;
+
+                if (btnAddRegKey.Text == "Add")
+                {
+                    MessageBox.Show("Successfully added BOOST_ROOT User and System Environment Variables.",
+                    Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Successfully replaced BOOST_ROOT User and System Environment Variables.",
+                    Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                txtStatus.Clear();
+                // txtStatus.AppendText("Successfully added or replaced BOOST_ROOT User and System Environment Variables." + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void AddUserVariable()
+        {
+            RegistryKey 
+ 
+            userVar = Registry.CurrentUser.CreateSubKey("Environment");
+
+            userVar.SetValue(variableName.Text, txtBoostPath.Text);
+            userVar.Close();
+        }
+
+        private void AddSystemVariable()
+        {
+            RegistryKey 
+
+            //systemVar = Registry.LocalMachine.CreateSubKey(@"SYSTEM\ControlSet001\Control\Session Manager\Environment");
+            systemVar = Registry.LocalMachine.CreateSubKey(@"SYSTEM\CurrentControlSet\Control\Session Manager\Environment");
+
+            systemVar.SetValue(variableName.Text, txtBoostPath.Text);
+            systemVar.Close();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                txtBoostPath.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private string CheckRegSystemStr()
+        {
+            string noSuch = (string)Registry.GetValue(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment",
+            "BOOST_ROOT", string.Empty);
+            return noSuch;
+        }
+
+        private string CheckRegUserStr()
+        {
+            string noSuch = (string)Registry.GetValue(@"HKEY_CURRENT_USER\Environment",
+            "BOOST_ROOT", string.Empty);
+            return noSuch;
+        }
+
+        // Check Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckRegSystemStr();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                checkBox1.Text = "Locked";
+                variableName.ReadOnly = true;
+            }
+            else
+            {
+                checkBox1.Text = "Unlocked";
+                variableName.ReadOnly = false;
+            }
+        }
+
+        private void GetRegData()
+        {
+            // user
+            RegistryKey regKeyUser = Registry.CurrentUser.OpenSubKey("Environment");
+            if (regKeyUser != null)
+            {
+                if (regKeyUser.GetValue(variableName.Text) != null)
+                {
+                    // MessageBox.Show("USER Variable - " + variableName.Text + " does exist");
+                    txtStatus.AppendText("User Variable " + variableName.Text + ", Path: \"" + CheckRegUserStr() + "\" exists on this computer." + Environment.NewLine);
+                    btnAddRegKey.Text = "Replace";
+                }
+
+                else
+                {
+                    //   MessageBox.Show("USER Variable - " + variableName.Text + " doesn't exist");
+                    txtStatus.AppendText("User Variable " + variableName.Text + " does NOT exist on this computer." + Environment.NewLine);
+                    btnAddRegKey.Text = "Add";
+                }
+            }
+
+            //system
+            RegistryKey regKeySystem = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Control\Session Manager\Environment");
+            if (regKeySystem != null)
+            {
+                if (regKeySystem.GetValue(variableName.Text) != null)
+                {
+                    // MessageBox.Show("System Variable - " + variableName.Text + " does exist");
+                    txtStatus.AppendText("System Variable " + variableName.Text + ", Path: \"" + CheckRegSystemStr() + "\" exists on this computer." + Environment.NewLine);
+
+                }
+
+                else
+                {
+                    //MessageBox.Show("System Variable - " + variableName.Text + " doesn't exist");
+                    txtStatus.AppendText("System Variable " + variableName.Text + " does NOT exist on this computer." + Environment.NewLine);
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkBox1.Checked = true;
+
+            GetRegData();
+        }
+
+        private void ChangeBackColor_Button(Button button, Color color)
+        {
+            button.BackColor = color;
+        }
+
+        private void btnAddRegKey_MouseEnter(object sender, EventArgs e)
+        {
+           btnAddRegKey.BackColor = Color.DodgerBlue;
+            btnAddRegKey.ForeColor = Color.White;
+        }
+
+        private void btnAddRegKey_MouseLeave(object sender, EventArgs e)
+        {
+            btnAddRegKey.BackColor = Color.SteelBlue;
+            btnAddRegKey.ForeColor = Color.White;
+        }
+
+        private void btnBrowse_MouseEnter(object sender, EventArgs e)
+        {
+            btnBrowse.BackColor = Color.DodgerBlue;
+            btnBrowse.ForeColor = Color.White;
+        }
+
+        private void btnBrowse_MouseLeave(object sender, EventArgs e)
+        {
+            btnBrowse.BackColor = Color.SteelBlue;
+            btnBrowse.ForeColor = Color.White;
+        }
+    }
+}
