dc143d498008507471ec096e4583c71cff9ac87f
mindsear <xxusername12843@yahoo.ro>
2018-05-27 17:36:31 +0300

    Changed Assembly Information

--- AddRegKey/Properties/AssemblyInfo.cs
+++ AddRegKey/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AddRegKey")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Add/Replace Boost Environment Variables")]
+[assembly: AssemblyDescription("Add/Replace Boost Environment Variables")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AddRegKey")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Mindsear")]
+[assembly: AssemblyProduct("Add/Replace Boost Environment Variables")]
+[assembly: AssemblyCopyright("Copyright Mindsear ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
